6c4a268870f7efa997483b03082ba1e52e4c8fec
Thiago Demas <thiago.demas7@gmail.com>
2020-09-03 16:03:26 -0300

    config .editorConfig, eslint, prettier

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+// src/server.ts
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello GoStack..'});
-});
+app.get('/', (request, response) =>
+    response.json({ message: 'Hello GoStack' }),
+);
 
 app.listen(3333, () => {
     console.log(' Server started on port 3333...');
