5bec55fb884a19f89872bcb51d8b96fb462a8c25
Thiago Demas <thiago.demas7@gmail.com>
2020-09-09 20:22:59 -0300

    Hash user password

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -15,6 +15,7 @@ usersRouter.post('/', async (request, response) => {
             email,
             password,
         });
+        delete user.password;
 
         return response.json(user);
     } catch (err) {
