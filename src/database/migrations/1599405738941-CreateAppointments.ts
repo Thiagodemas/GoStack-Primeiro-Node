4818258350fedadc8ccfc44c88588a589924f6c8
Thiago Demas <thiago.demas7@gmail.com>
2020-09-08 22:25:38 -0300

    Define appointment relationship to user (provider)

--- src/database/migrations/1599405738941-CreateAppointments.ts
+++ src/database/migrations/1599405738941-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1599405738941
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
