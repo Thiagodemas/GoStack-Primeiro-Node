57ce97d35d1e64927c067e79a72ec214bf1db8e3
Thiago Demas <thiago.demas7@gmail.com>
2020-09-11 16:40:55 -0300

    Update user avatar field

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,14 +2,18 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-export default {
-    storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'tmp'),
-        filename(reuqest, file, callback){
-            const fileHash = crypto.randomBytes(10).toString('hex');
-            const fileName = `${fileHash}-${file.originalname}`;
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
-            return callback(null, fileName);
-        }
-    })
-}
+export default {
+  directory: tmpFolder,
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
