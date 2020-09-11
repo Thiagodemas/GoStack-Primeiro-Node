62ba4795a73370055378f110af3ceb1b69e78cec
Thiago Demas <thiago.demas7@gmail.com>
2020-09-11 15:59:29 -0300

    Enable user to upload an avatar image

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,11 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
-
 interface TokenPayload {
-    iat: number,
-    exp: number,
-    sub: string
+    iat: number;
+    exp: number;
+    sub: string;
 }
 
 export default function ensureAuthenticated(
@@ -25,7 +24,12 @@ export default function ensureAuthenticated(
     try {
         const decoded = verify(token, authConfig.jwt.secret);
 
-        console.log(decoded);
+        const { sub } = decoded as TokenPayload;
+
+        request.user = {
+            id: sub,
+        };
+
         return next();
     } catch {
         throw new Error('Invalid JWT token');
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,26 +1,23 @@
-import { Router } from 'express';
+import { Router, response } from 'express';
+import multer from 'multer';
 
-import CreateUserService from '../services/CreateUserServices';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+usersRouter.post('/', async (request, response) => {});
 
-        const createUser = new CreateUserService();
-
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        return response.json({
+            ok: true,
         });
-        delete user.password;
-
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.massage });
-    }
-});
+    },
+);
 
 export default usersRouter;
